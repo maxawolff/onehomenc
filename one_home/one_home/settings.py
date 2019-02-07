81478f53df43f2c9234a29a278d9b835d72cd672
maxawolff <maxawolff@hotmail.com>
2019-02-07 09:42:55 -0800

    modifying initial settings.py to remove hardcoded variables and place into environment

--- one_home/one_home/settings.py
+++ one_home/one_home/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '09go(x!18*)n&jb+x7dk74vkx7deln(b(4oho(-1+ua%hmx8p8'
+SECRET_KEY = os.environ.get("SECRET_KEY", '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'one_home.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': 5432,
+        'TEST': {'NAME': os.environ.get("TEST_DB", '')},
     }
 }
 
