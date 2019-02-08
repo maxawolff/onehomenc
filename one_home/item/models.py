e2fdffe2b639d56031fb1d4b9a81dd8a86bc2e88
maxawolff <maxawolff@hotmail.com>
2019-02-08 08:28:23 -0800

    working on models for item and room

--- one_home/item/models.py
+++ one_home/item/models.py
@@ -1,3 +1,26 @@
+"""Model for items to be purchased."""
 from django.db import models
 
-# Create your models here.
+
+class Room(models.Model):
+    """.Model for room of a house, will contain items."""
+
+    name = models.CharField(max_length=50)
+
+
+class Item(models.Model):
+    """Model for item in store."""
+
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000,
+                                   null=True,
+                                   blank=True)
+    # amount_needed = models.IntegerField()
+    # amount_purchased = models.IntegerField()
+    # use these if we want multiple of same item to be one model
+    purchased = models.BooleanField(default=False)
+    cost = models.FloatField()
+    for_room = models.ForeignKey(Room,
+                                 onDelete=models.Cascade,
+                                 blank=True,
+                                 null=True)
