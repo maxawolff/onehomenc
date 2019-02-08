f4a0c275a619309ef19b9e38fe62e9f4b37daa44
maxawolff <maxawolff@hotmail.com>
2019-02-08 12:28:13 -0800

    finished a basic home route, page will now load and display correctly. Nothing is yet displayed but the route is functional, next to add items to page

--- one_home/one_home/urls.py
+++ one_home/one_home/urls.py
@@ -1,4 +1,4 @@
-"""one_home URL Configuration
+"""one_home URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from one_home.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name='homepage')
 ]
--- one_home/one_home/views.py
+++ one_home/one_home/views.py
@@ -1,8 +1,15 @@
 """Views for one home website."""
 from django.views.generic import TemplateView
+import pdb
 
 
 class HomeView(TemplateView):
     """Function to display the home page."""
 
-    pass
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        """Get specific data for homepage."""
+        context = super(HomeView, self).get_context_data(**kwargs)
+        pdb.set_trace()
+        return context
