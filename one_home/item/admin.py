a4adcebd6bb97b0b02367f4281aceb682f1bf55e
maxawolff <maxawolff@hotmail.com>
2019-02-19 11:40:15 -0800

    mograted database to apply updates to models, registered cart item to admin page

--- one_home/item/admin.py
+++ one_home/item/admin.py
@@ -1,9 +1,10 @@
 """Adds models to the admin page."""
 from django.contrib import admin
-from .models import Item, Room, Buyer
+from .models import Item, Room, Buyer, Cart
 
 # Register your models here.
 
 admin.site.register(Item)
 admin.site.register(Room)
 admin.site.register(Buyer)
+admin.site.register(Cart)
